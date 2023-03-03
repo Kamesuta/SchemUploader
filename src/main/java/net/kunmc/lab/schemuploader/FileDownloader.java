a673f7160a1ceb1e63b848af537c7b443ddaa828
Kamesuta <kamesuta@gmail.com>
2023-03-04 04:27:25 +0900

    schemファイルかどうか検証する

--- src/main/java/net/kunmc/lab/schemuploader/FileDownloader.java
+++ src/main/java/net/kunmc/lab/schemuploader/FileDownloader.java
@@ -5,10 +5,13 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.ResponseBody;
 import okio.BufferedSink;
+import okio.BufferedSource;
 import okio.Okio;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.zip.GZIPInputStream;
 
 /**
  * ファイルをダウンロードするクラス
@@ -70,17 +73,52 @@ public class FileDownloader {
                     return result;
                 }
                 // ダウンロードしたファイルを保存
+                boolean shouldDelete = false;
                 try (BufferedSink sink = Okio.buffer(Okio.sink(file))) {
                     sink.writeAll(body.source());
+
+                    // Schematicファイルかどうかをチェックする
+                    if (!checkSchematic(sink.buffer())) {
+                        result.error = "Schematicファイルではありません";
+                        shouldDelete = true;
+                        return result;
+                    }
 
                     // 結果を構築して返す
                     result.success = true;
                     return result;
+                } finally {
+                    // ダウンロードに失敗したらファイルを削除する
+                    if (shouldDelete) {
+                        file.delete();
+                    }
                 }
             }
         } catch (IOException e) {
             result.error = String.format("IOエラー: %s", e.getMessage());
             return result;
         }
+    }
+
+    /**
+     * Schematicファイルかどうかをチェックする
+     *
+     * @param source ファイルのソース
+     * @return チェック結果
+     */
+    private static boolean checkSchematic(BufferedSource source) {
+        // Schematicファイルかどうかをチェックする
+        try (GZIPInputStream input = new GZIPInputStream(source.inputStream())) {
+            // ヘッダーを読み込む
+            byte[] header = new byte[12];
+            byte[] expected = {0x0a, 0x00, 0x09, 'S', 'c', 'h', 'e', 'm', 'a', 't', 'i', 'c'};
+            // ヘッダーが一致しなかったら失敗
+            if (input.read(header) < expected.length || !Arrays.equals(header, expected)) {
+                return false;
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 }
